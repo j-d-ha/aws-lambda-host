c87dc9dc2cb50f0077818bea006087b5086adc23
Jonas Ha <jonas-ha@outlook.com>
2025-11-09 09:50:46 -0500

    feat(example): implement OpenTelemetry instrumentation and tracing
    Added `Instrumentation` and `Service` classes to handle activity tracing. Updated `Program.cs` to configure OpenTelemetry with AWS Lambda support and OTLP exporter. Adjusted project file to include necessary dependencies and configurations for OpenTelemetry setup.

--- examples/AwsLambda.Host.Example.OpenTelemetry/Program.cs
+++ examples/AwsLambda.Host.Example.OpenTelemetry/Program.cs
@@ -1,3 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using AwsLambda.Host;
+using AwsLambda.Host.Example.OpenTelemetry;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Instrumentation.AWSLambda;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
-Console.WriteLine("Hello, World!");
+var builder = LambdaApplication.CreateBuilder();
+
+builder.Services.AddScoped<IService, Service>();
+builder.Services.AddSingleton<Instrumentation>();
+
+builder
+    .Services.AddOpenTelemetry()
+    .WithTracing(configure =>
+        configure
+            .AddAWSLambdaConfigurations()
+            .AddSource("MyLambda")
+            .SetResourceBuilder(
+                ResourceBuilder.CreateDefault().AddService("MyLambda", serviceVersion: "1.0.0")
+            )
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri("http://localhost:4318/v1/traces");
+                options.Protocol = OtlpExportProtocol.HttpProtobuf;
+            })
+    );
+
+var lambda = builder.Build();
+
+lambda.UseOpenTelemetryTracing();
+
+lambda.MapHandler(
+    async (
+        [Event] Request request,
+        IService service,
+        Instrumentation instrumentation,
+        CancellationToken cancellationToken
+    ) =>
+    {
+        using var activity = instrumentation.ActivitySource.StartActivity("Handler");
+
+        var message = await service.GetMessage(request.Name, cancellationToken);
+
+        return new Response(message, DateTime.UtcNow);
+    }
+);
+
+await lambda.RunAsync();
+
+internal record Request(string Name);
+
+internal record Response(string Message, DateTime Timestamp);
