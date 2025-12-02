fb20c626c92f3fcaa3c9f37ea61e5e6b9cffeeeb
Jonas Ha <jonas-ha@outlook.com>
2025-12-02 18:34:54 -0500

    refactor(core): simplify `DefaultFeatureCollectionFactory` constructor
    - Replaced internal field initialization with constructor parameter assignment.
    - Removed null-checking and filtering logic as it's no longer necessary.
    - Updated `Create` method to use the constructor-initialized `providers`.

--- src/AwsLambda.Host/Core/Features/DefaultFeatureCollectionFactory.cs
+++ src/AwsLambda.Host/Core/Features/DefaultFeatureCollectionFactory.cs
@@ -1,12 +1,8 @@
 namespace AwsLambda.Host.Core;
 
-internal class DefaultFeatureCollectionFactory : IFeatureCollectionFactory
+internal class DefaultFeatureCollectionFactory(IEnumerable<IFeatureProvider> providers)
+    : IFeatureCollectionFactory
 {
-    private readonly IEnumerable<IFeatureProvider> _featureProviders;
-
-    public DefaultFeatureCollectionFactory(IEnumerable<IFeatureProvider?> featureProviders) =>
-        _featureProviders = featureProviders.Where(x => x is not null).Select(x => x!).ToArray();
-
     public IFeatureCollection Create(IEnumerable<IFeatureProvider> featureProviders) =>
-        new DefaultFeatureCollection(_featureProviders.Concat(featureProviders).ToArray());
+        new DefaultFeatureCollection(providers.Concat(featureProviders).ToArray());
 }
