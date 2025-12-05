d71423374a970d522e6ea2be2bf34da5494e606b
Jonas Ha <jonas-ha@outlook.com>
2025-12-05 16:32:17 -0500

    feat(testing): add LambdaBootstrapRequest model for request handling
    - Introduced `LambdaBootstrapRequest` model to encapsulate Lambda runtime request data.
    - Added `RequestType`, `RequestMessage`, and `RouteValue` properties as required fields.
    - Implemented `RequestId` property using `RouteValueDictionary` for request identification.

---
+++ src/AwsLambda.Host.Testing/Models/LambdaBootstrapRequest.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Routing;
+
+namespace AwsLambda.Host.Testing;
+
+internal class LambdaBootstrapRequest
+{
+    internal required RequestType RequestType { get; init; }
+    internal required HttpRequestMessage RequestMessage { get; init; }
+    internal required RouteValueDictionary RouteValue { get; init; }
+
+    internal string RequestId
+    {
+        get
+        {
+            field ??=
+                RouteValue.TryGetValue("RequestId", out var requestId)
+                && requestId is string requestIdString
+                    ? requestIdString
+                    : string.Empty;
+
+            return field;
+        }
+    }
+}
