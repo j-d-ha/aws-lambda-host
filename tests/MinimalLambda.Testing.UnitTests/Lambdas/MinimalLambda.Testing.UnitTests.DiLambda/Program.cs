2ddb9f40bb25c1858d3e22aa2819a352a8eed159
Jonas Ha <jonas-ha@outlook.com>
2025-12-12 11:38:09 -0500

    feat(DiLambda): implement dependency injection and lifecycle management
    - Added `ILifecycleService` and `IService` interfaces with corresponding implementations.
    - Integrated `Startup` and `Shutdown` hooks using `OnInit` and `OnShutdown` methods.
    - Enhanced handler to process `Request` and generate `Response` with timestamp.
    - Updated `Program.cs` to register services and manage lifecycle events.

--- tests/MinimalLambda.Testing.UnitTests/Lambdas/MinimalLambda.Testing.UnitTests.DiLambda/Program.cs
+++ tests/MinimalLambda.Testing.UnitTests/Lambdas/MinimalLambda.Testing.UnitTests.DiLambda/Program.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MinimalLambda.Builder;
 
 var builder = LambdaApplication.CreateBuilder();
 
+builder.Services.AddSingleton<ILifecycleService, LifecycleService>();
+builder.Services.AddSingleton<IService, Service>();
+
 await using var lambda = builder.Build();
 
-lambda.MapHandler(([Event] string name) => $"Hello {name}!");
+lambda.OnInit((ILifecycleService service) => service.OnStart());
+
+lambda.MapHandler(
+    ([Event] Request request, IService service) =>
+        new Response(service.GetMessage(request.Name), DateTime.UtcNow)
+);
+
+lambda.OnShutdown((ILifecycleService service) => service.OnStop());
 
 await lambda.RunAsync();
 
 public class DiLambda;
+
+internal record Request(string Name);
+
+internal record Response(string Message, DateTime TimestampUtc);
+
+internal interface IService
+{
+    string GetMessage(string name);
+}
+
+internal class Service : IService
+{
+    public string GetMessage(string name) => $"Hello {name}!";
+}
+
+internal interface ILifecycleService
+{
+    bool OnStart();
+    void OnStop();
+}
+
+internal class LifecycleService : ILifecycleService
+{
+    public bool OnStart() => true;
+
+    public void OnStop() { }
+}
