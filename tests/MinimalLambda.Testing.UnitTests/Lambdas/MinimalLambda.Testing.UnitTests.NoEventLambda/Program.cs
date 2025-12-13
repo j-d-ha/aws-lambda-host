1442e9eac4eb4b85401ed41e5ab95040912ac65b
Jonas Ha <jonas-ha@outlook.com>
2025-12-12 21:27:38 -0500

    feat(tests): enhance `NoEventLambda` configuration and add new test case
    - Updated `NoEventLambda` to utilize `IConfiguration` for dynamic message handling in the handler.
    - Added a new test, `NoEvent_ConfigurationCanBeOverwritten`, to validate custom configuration support.
    - Introduced in-memory configuration dictionary for improved test flexibility and coverage.

--- tests/MinimalLambda.Testing.UnitTests/Lambdas/MinimalLambda.Testing.UnitTests.NoEventLambda/Program.cs
+++ tests/MinimalLambda.Testing.UnitTests/Lambdas/MinimalLambda.Testing.UnitTests.NoEventLambda/Program.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using MinimalLambda.Builder;
 
 var builder = LambdaApplication.CreateBuilder();
 
+builder.Configuration.AddInMemoryCollection(
+    new Dictionary<string, string> { ["MESSAGE"] = "Hello World!" }!
+);
+
 await using var lambda = builder.Build();
 
-lambda.MapHandler(() => new NoEventLambdaResponse("Hello World!", DateTime.UtcNow));
+lambda.MapHandler(
+    (IConfiguration configuration) =>
+        new NoEventLambdaResponse(configuration["MESSAGE"] ?? "NOT_FOUND", DateTime.UtcNow)
+);
 
 await lambda.RunAsync();
 
--- tests/MinimalLambda.Testing.UnitTests/Tests/MinimalLambda.Testing.UnitTests/NoEventLambdaTests.cs
+++ tests/MinimalLambda.Testing.UnitTests/Tests/MinimalLambda.Testing.UnitTests/NoEventLambdaTests.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Configuration;
+
 namespace MinimalLambda.Testing.UnitTests;
 
 public class NoEventLambdaTests : IClassFixture<LambdaApplicationFactory<NoEventLambda>>
@@ -21,5 +23,29 @@ public class NoEventLambdaTests : IClassFixture<LambdaApplicationFactory<NoEvent
         response.WasSuccess.Should().BeTrue();
         response.Response.Should().NotBeNull();
         response.Response.Message.Should().Be("Hello World!");
+    }
+
+    [Fact]
+    public async Task NoEvent_ConfigurationCanBeOverwritten()
+    {
+        var factory = new LambdaApplicationFactory<NoEventLambda>()
+            .WithCancelationToken(TestContext.Current.CancellationToken)
+            .WithHostBuilder(builder =>
+                builder.ConfigureAppConfiguration(
+                    (_, config) =>
+                        config.AddInMemoryCollection(
+                            new Dictionary<string, string> { ["MESSAGE"] = "Hello Mars!" }!
+                        )
+                )
+            );
+
+        var response = await factory.TestServer.InvokeNoEventAsync<NoEventLambdaResponse>(
+            TestContext.Current.CancellationToken
+        );
+
+        response.Should().NotBeNull();
+        response.WasSuccess.Should().BeTrue();
+        response.Response.Should().NotBeNull();
+        response.Response.Message.Should().Be("Hello Mars!");
     }
 }
